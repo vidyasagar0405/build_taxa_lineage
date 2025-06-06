e3f0f32efa860d7359cc38234685e4ed3e8c2559
vidyasagar0405 <vidyasagar0405@gmail.com>
2025-06-06 12:24:38 +0530

    updated init

--- src/build_taxa_lineage/__init__.py
+++ src/build_taxa_lineage/__init__.py
@@ -7,17 +7,17 @@ __author__ = "Vidyasagar"
 __copyright__ = "Copyright 2025, Vidyasagar"
 __credits__ = ["Vidyasagar"]
 __maintainer__ = "Vidyasagar"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __licence__ = "MIT"
 
 ##############################################################################
 # Local imports.
 
-from build_taxa_lineage.build_taxa_line import build_lineage
+from build_taxa_lineage.build_taxa_line import build_lineage, build_lineage_map
 
 ##############################################################################
 # Export the imports.
 
-__all__ = ["build_lineage"]
+__all__ = ["build_lineage", "build_lineage_map"]
 
 ### __init__.py ends here
